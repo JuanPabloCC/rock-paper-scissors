242b6b13645b58bd84f4c5082a357f2c59b7d125
Juan Pablo <juan.pablo.castano@outlook.com>
2025-06-04 08:49:31 -0500

    feat: add getComputerChoice function to js script
    Introduce getComputerChoice function which randomnly
    returns "Paper", "Rock" or "Scissors".

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice
+    
+    switch (randomNumber) {
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        default:
+            computerChoice = "Scissors";
+            break;
+    }
+
+    return computerChoice
+}
