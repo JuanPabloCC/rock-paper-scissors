b2b3804875ae8a02c768e9ea16620baa69050f49
Juan Pablo <juan.pablo.castano@outlook.com>
2025-06-04 09:54:58 -0500

    feat: add playGame function to play 5 rounds in a row

--- script.js
+++ script.js
@@ -22,31 +22,51 @@ function getHumanChoice() {
     return humanChoice
 }
 
-let computerScore = 0;
-let humanScore = 0;
+function playGame(){
 
-function playRound(computerChoice, humanChoice) {
-    humanChoice = humanChoice.toLowerCase();
+    let computerScore = 0;
+    let humanScore = 0;
 
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore += 1;
-        return `You win! ${humanChoice} beats ${computerChoice}.`
-    } else if (humanChoice === "rock" && computerChoice === "paper"){
-        computerScore += 1;
-        return `You lose! ${computerChoice} beats ${humanChoice}.`
-    } else if (humanChoice === "scissors" && computerChoice === "paper"){
-        humanScore += 1;
-        return `You win! ${humanChoice} beats ${computerChoice}.`
-    } else if (humanChoice === "scissors" && computerChoice === "rock"){
-        computerScore += 1;
-        return `You lose! ${computerChoice} beats ${humanChoice}.`
-    } else if (humanChoice === "paper" && computerChoice === "rock"){
-        humanScore += 1;
-        return `You win! ${humanChoice} beats ${computerChoice}.`
-    } else if (humanChoice === "paper" && computerChoice === "scissors"){
-        computerScore += 1;
-        return `You lose! ${computerChoice} beats ${humanChoice}.`
-    } else {
-        return "It's a draw! Try again."
+    function playRound(computerChoice, humanChoice) {
+        humanChoice = humanChoice.toLowerCase();
+
+        if (humanChoice === "rock" && computerChoice === "scissors") {
+            humanScore += 1;
+            return `You win! ${humanChoice} beats ${computerChoice}.`
+        } else if (humanChoice === "rock" && computerChoice === "paper"){
+            computerScore += 1;
+            return `You lose! ${computerChoice} beats ${humanChoice}.`
+        } else if (humanChoice === "scissors" && computerChoice === "paper"){
+            humanScore += 1;
+            return `You win! ${humanChoice} beats ${computerChoice}.`
+        } else if (humanChoice === "scissors" && computerChoice === "rock"){
+            computerScore += 1;
+            return `You lose! ${computerChoice} beats ${humanChoice}.`
+        } else if (humanChoice === "paper" && computerChoice === "rock"){
+            humanScore += 1;
+            return `You win! ${humanChoice} beats ${computerChoice}.`
+        } else if (humanChoice === "paper" && computerChoice === "scissors"){
+            computerScore += 1;
+            return `You lose! ${computerChoice} beats ${humanChoice}.`
+        } else {
+            return "It's a draw! Try again."
+        }
     }
+
+    for (let step = 0; step < 5; step++){
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+        playRound(computerSelection, humanSelection);
+    }
+
+    if (humanScore > computerScore){
+        return alert(`You won! :)\nHuman Score: ${humanScore} - ComputerScore: ${computerScore}`);
+    } else if (humanScore === computerScore){
+        return alert(`It's a draw :0!\nHuman Score: ${humanScore} - ComputerScore: ${computerScore}`);
+    } else {
+        return alert(`You lose! :(\nHuman Score: ${humanScore} - ComputerScore: ${computerScore}`);
+    }
+    
 }
+
+playGame();
